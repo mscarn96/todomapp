625c6d3c5452e6092710a0fb01e1f3a56ef62869
mscarn96 <falkowskialbert@gmail.com>
2021-04-17 12:06:11 +0200

    add:security packages

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+
 const cookieParser = require("cookie-parser");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
@@ -21,6 +28,22 @@ const router = express.Router();
 app.use(cookieParser());
 
 app.use(express.json());
+
+app.use(mongoSanitize());
+
+app.use(helmet());
+
+app.use(xss());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
+app.use(hpp());
+
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
